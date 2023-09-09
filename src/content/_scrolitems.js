5b7c6f8ef529d9e6cd74e43732533b0021ea1d6c
lycan <snehal1380@gmail.com>
2023-09-10 03:37:04 +0530

    about updated

---
+++ src/content/_scrolitems.js
@@ -0,0 +1,58 @@
+export const scrollItems = {
+  10: { category: null, value: "I'am Snehal" },
+  12: { category: null, value: "Web developer" },
+  14: { category: "What I like", value: "Anime" },
+  16: { category: "What I like", value: "Movie" },
+  18: { category: "What I like", value: "Nature" },
+  20: { category: "What I like", value: "Philosopher" },
+  22: { category: "What I like", value: "Science" },
+  24: { category: "What I like", value: "Technology" },
+  29: {
+    category: "Diploma in computer engineering",
+    value: "Institute of Diploma studies",
+  },
+  33: {
+    category: "Diploma in computer engineering",
+    value: "Nirma university - 2015",
+  },
+  35: { category: "Computer engineering", value: "C.k. pithawala" },
+  37: {
+    category: "Computer engineering",
+    value: "Gujarat technological university - 2020",
+  },
+  40: {
+    category: "I work at",
+    value: "Sarvadhi Solutions Private Limited",
+  },
+  43: { category: "I work at", value: "3 month internship" },
+  48: { category: "I work at", value: "7 month work" },
+  51: { category: "Technology I work on", value: "React" },
+  55: { category: "Technology I work on", value: "Electronjs" },
+  67: { category: "Things I do", value: "Exploring the outdoors" },
+  69: {
+    category: "Things I do",
+    value: "Participating in artistic activities",
+  },
+  71: { category: "Things I do", value: "Watching anime" },
+  72: { category: "Things I do", value: "Watching movie" },
+  74: {
+    category: "Things I do",
+    value: "Keeping up with technology advancements",
+  },
+  76: {
+    category: "Things I do",
+    value: "Traveling and experiencing new cultures",
+  },
+  78: {
+    category: "Things I do",
+    value: "Volunteer work or community service",
+  },
+  80: {
+    category: "Things I do",
+    value: "Bringing ideas to life through web development",
+  },
+  84: { category: "Looking for", value: "Enthusiastic" },
+  86: { category: "Looking for", value: "Passionate" },
+  88: { category: "Looking for", value: "Detail-Oriented" },
+  90: { category: "Looking for", value: "Continuous Learner" },
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode:"class",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--gradient-color-stops))",
+      colors: {
+        bgcolor: "#0E2A47",
+        primary: "#EDD5CA",
+        secondary: "#2AB8BF",
+        "blue-zodiac": {
+          50: "#f1f8fe",
+          100: "#e2effc",
+          200: "#c0def7",
+          300: "#87c3f2",
+          400: "#48a6e8",
+          500: "#208ad7",
+          600: "#126db7",
+          700: "#105794",
+          800: "#114a7b",
+          900: "#143f66",
+          950: "#0e2a47",
+        },
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": {
+            transform: "rotate(-3deg)",
+          },
+          "50%": {
+            transform: "rotate(3deg)",
+          },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
       },
     },
   },
